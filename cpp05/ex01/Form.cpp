1e352827302fb5645281bf5ebad7dcf45b8b31ce
minsikim-42 <spalstlr321@naver.com>
2022-04-08 16:23:58 +0900

    BUE: zandi

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -1,31 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Form.hpp                                           :+:      :+:    :+:   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minsikim <minsikim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/07 17:13:07 by minsikim          #+#    #+#             */
-/*   Updated: 2022/04/08 15:37:34 by minsikim         ###   ########.fr       */
+/*   Created: 2022/04/08 15:36:25 by minsikim          #+#    #+#             */
+/*   Updated: 2022/04/08 15:43:44 by minsikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FORM_HPP
-# define FORM_HPP
-# include <iosteam>
+#includ "Form.hpp"
 
+Form::Form()
+{
 
-class Form{
-private:
-	const std::string	name;
-	bool	is_signed;
-	const int	sign_grade;
-	const int	execute_grade;
+}
 
-public:
-	Form();
-	Form(const Form &origin);
-	~Form();
-	Form	&operator=(const Form &origin);
-	Form::GradeTooHighException
+Form::Form(const Form &origin)
+{
+	this->name = origin.get_name();
+	this->is_signed = origin.is_signed = false;
+	this->sign_grade = origin.get_sign();
+	this-> execute_grade = origin.get_execute();
+	return *this;
+}
+
+Form::~Form()
+{
+
+}
+
+Form	&Form::operator=(const Form &)
+{
+	return *this;
 }
