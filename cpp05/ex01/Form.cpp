40155f08e2d9fd09375b710ff32f1fd6716fee90
minsikim-42 <spalstlr321@naver.com>
2022-04-09 14:27:56 +0900

    MOVE: to Mac

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -3,27 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   Form.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minsikkim <minsikkim@student.42.fr>        +#+  +:+       +#+        */
+/*   By: minsikim <minsikim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 15:36:25 by minsikim          #+#    #+#             */
-/*   Updated: 2022/04/09 00:41:38 by minsikkim        ###   ########.fr       */
+/*   Updated: 2022/04/09 11:38:25 by minsikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Form.hpp"
 
-Form::Form()
+Form::Form() : name(""), is_signed(false), sign_grade(0), execute_grade(0)
 {
 
 }
 
-Form::Form(const Form &origin)
+Form::Form(const Form &origin) : name(origin.get_name()), is_signed(false), sign_grade(origin.get_sign_grade()), execute_grade(origin.get_execute_grade())
 {
-	this->name = origin.get_name();
-	this->is_signed = origin.is_signed = false;
-	this->sign_grade = origin.get_sign();
-	this-> execute_grade = origin.get_execute();
-	return *this;
+	
 }
 
 Form::~Form()
@@ -34,4 +30,24 @@ Form::~Form()
 Form	&Form::operator=(const Form &)
 {
 	return *this;
+}
+
+std::string	Form::get_name(void) const
+{
+	return (this->name);
+}
+
+bool		Form::get_signed(void) const
+{
+	return (is_signed);
+}
+
+const int	Form::get_sign_grade(void) const
+{
+	return (sign_grade);
+}
+
+const int	Form::get_execute_grade(void) const
+{
+	return (execute_grade);
 }
--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -6,13 +6,13 @@
 /*   By: minsikim <minsikim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:13:07 by minsikim          #+#    #+#             */
-/*   Updated: 2022/04/08 15:37:34 by minsikim         ###   ########.fr       */
+/*   Updated: 2022/04/09 12:07:45 by minsikim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FORM_HPP
 # define FORM_HPP
-# include <iosteam>
+# include <iostream>
 
 
 class Form{
@@ -27,5 +27,18 @@ public:
 	Form(const Form &origin);
 	~Form();
 	Form	&operator=(const Form &origin);
-	Form::GradeTooHighException
-}
+
+	std::string	get_name(void) const;
+	bool		get_signed(void) const;
+	const int	get_sign_grade(void) const;
+	const int	get_execute_grade(void) const;
+
+	class	GradeTooHighException : public std::exception
+	{
+	public:
+		virtual const char *what() const throw();
+	};
+	class	GradeTooLowException;
+};
+
+#endif
