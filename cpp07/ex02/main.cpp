0015e4240da165d9485c198c687ad83cb3a76857
minsikim-42 <spalstlr321@naver.com>
2022-04-18 20:36:51 +0900

    xx07 main

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -80,27 +80,78 @@ void	test2()
 	delete ar;
 }
 
-int main()
+#define MAX_VAL 750
+int main(int, char**)
 {
-	test();
-	test2();
-	std::cout << "----------------\n";
-	std::cout << "new initial test\n";
-	std::cout << "----------------\n";
-	int *ini = new int[5];
-	for (int i=0; i<5; i++)
-		std::cout << (ini)[i] << std::endl;
-	int *a = new int[5];
-	for (int i=0; i < 5; i++)
-		(a)[i] = i;
-	int *b = new int[5];
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
 
-	for (int i=0; i<5; i++)
-		std::cout << (a)[i] << " " << (b)[i] << std::endl;
-	
-	delete[] ini;
-	delete[] a;
-	delete[] b;
-	system("leaks template");
-	return (0);
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
 }
+
+// int main()
+// {
+// 	test();
+// 	test2();
+// 	std::cout << "----------------\n";
+// 	std::cout << "new initial test\n";
+// 	std::cout << "----------------\n";
+// 	int *ini = new int[5];
+// 	for (int i=0; i<5; i++)
+// 		std::cout << (ini)[i] << std::endl;
+// 	int *a = new int[5];
+// 	for (int i=0; i < 5; i++)
+// 		(a)[i] = i;
+// 	int *b = new int[5];
+
+// 	for (int i=0; i<5; i++)
+// 		std::cout << (a)[i] << " " << (b)[i] << std::endl;
+	
+// 	delete[] ini;
+// 	delete[] a;
+// 	delete[] b;
+// 	system("leaks template");
+// 	return (0);
+// }
